74392a8ca203da6bec02c476684aae01772da4cf
Hoi Dan IT with Eric <haryphamdev@gmail.com>
2022-02-11 11:21:15 +0700

    video 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import logo from './logo.svg'
 import './App.css'
+import MyComponent from './components/MyComponent'
 
 function App() {
 
   //jsx  
   const name = { name: "eric" }
   return (
-    <div className="App hoidanit">
+    <div className="App hoidanit" style={{ color: 'red' }}>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello world with React and {JSON.stringify(name)}</p>
+        <MyComponent />
       </header>
     </div>
   )
