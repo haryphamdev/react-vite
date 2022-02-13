7b423c0bdb463b9226da851107ef325b675aa54c
Hoi Dan IT with Eric <haryphamdev@gmail.com>
2022-02-13 09:33:28 +0700

    video 05

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import logo from './logo.svg'
 import './App.css'
 import MyComponent from './components/MyComponent'
+import { useState } from 'react'
 
 function App() {
 
   //jsx  
-  const name = { name: "eric" }
+  const [name, setName] = useState("hoi dan it");
+
   return (
     <div className="App hoidanit" style={{ color: 'red' }}>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello world with React and {JSON.stringify(name)}</p>
+        <input type="text" value={name} />
+        <p>Hello world with React and {name}</p>
         <MyComponent />
       </header>
     </div>
