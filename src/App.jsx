7bd07992318e3320f2c023e83de4dd2db884ed70
Hoi Dan IT with Eric <haryphamdev@gmail.com>
2022-02-13 09:43:55 +0700

    video 6

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,18 @@ import { useState } from 'react'
 function App() {
 
   //jsx  
-  const [name, setName] = useState("hoi dan it");
-
+  const [name, setName] = useState("");
+  const [infor, setInfor] = useState("");
+  const handleOnClick = () => {
+    setInfor(name)
+  }
   return (
     <div className="App hoidanit" style={{ color: 'red' }}>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <input type="text" value={name} />
-        <p>Hello world with React and {name}</p>
+        <input type="text" value={name} onChange={(event) => setName(event.target.value)} />
+        <button onClick={() => handleOnClick()}>Submit</button>
+        <p>Hello world with React and {infor}</p>
         <MyComponent />
       </header>
     </div>
